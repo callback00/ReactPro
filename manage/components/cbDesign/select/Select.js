abfa7e7d103926322cd611a2ccc9b41c7a6ec00b
dl <344212106@qq.com>
2018-04-10 12:39:57 +0800

    1、设置字体全局 2、select控件添加查询功能

--- manage/components/cbDesign/select/Select.js
+++ manage/components/cbDesign/select/Select.js
@@ -11,26 +11,80 @@ class Select extends React.Component {
 
             openFlag: false,
         }
+
+        //this.props.showSearch 为真时需要多处理一步
+        this.mouseLeaveContent = true
+
+        // 在下拉菜单点击会触发onBlur事件，所以用这个标记一下
+        this.mouseLeaveDropdown = true
+
+        //防止在触发隐藏时连续点击选择值
+        this.selecting = false
+
+        //this.props.showSearch 为真时， 输入数据时隐藏原文本显示框
+        this.hiddenTextValueFlag = false
+
+        //如果showSearch 存在需要清空input的值
+        this.inputEl = null
+
+        this.textValueEl = null
     }
 
     onClick(e) {
         let flag = this.state.openFlag
         flag = !flag
+
+        if (this.props.showSearch) {
+            // 清空input的内容
+            this.inputEl.value = ''
+            this.hiddenTextValueFlag = false
+            this.textValueEl.className = 'cbd-select-content-textvalue'
+        }
+
         this.setState({
-            openFlag: flag
+            openFlag: flag,
+        }, () => {
+            if (this.props.showSearch) {
+                this.inputEl.focus();
+            }
         })
     }
 
     onBlur(e) {
-        this.setState({
-            openFlag: false
-        })
+        if (this.mouseLeaveDropdown && this.mouseLeaveContent) {
+            this.setState({
+                openFlag: false
+            })
+
+            if (this.props.showSearch) {
+                // 清空input的内容
+                this.inputEl.value = ''
+
+                this.hiddenTextValueFlag = false
+                this.textValueEl.className = 'cbd-select-content-textvalue'
+            }
+        }
     }
 
     onSelect(e) {
         let flag = this.state.openFlag
         const el = e.target
+
+        if (el.className.indexOf("disabled") !== -1) {
+            return
+        }
         if (el.localName === 'li') {
+
+            if (this.selecting) {
+                return
+            }
+
+            if (this.props.showSearch) {
+                this.hiddenTextValueFlag = false
+                this.textValueEl.className = 'cbd-select-content-textvalue'
+            }
+
+            this.selecting = true
             const value = el.outerText
             const key = el.dataset.key
 
@@ -39,17 +93,19 @@ class Select extends React.Component {
                 if (el === element) {
                     element.className = element.className + ' select'
                 } else {
-                    element.className = ''
+                    if (element.className.indexOf("disabled") < 0) {
+                        element.className = 'cbd-select-dropdown-li'
+                    }
                 }
             });
+            this.setState({
+                value: { value, key },
+                openFlag: false,
+            })
 
             setTimeout(() => {
-                this.setState({
-                    value: { value, key },
-                    openFlag: false,
-                })
-            }, 20)
-
+                this.selecting = false
+            }, 500)
         } else {
             flag = !flag
             this.setState({
@@ -58,12 +114,42 @@ class Select extends React.Component {
         }
     }
 
+    onMouseLeave(e) {
+        this.mouseLeaveDropdown = true
+    }
+
+    onMouseEnter(e) {
+        this.mouseLeaveDropdown = false
+    }
+
+    onMouseLeaveContent(e) {
+        this.mouseLeaveContent = true
+    }
+
+    onMouseEnterContent(e) {
+        this.mouseLeaveContent = false
+    }
+
+    inputOnChange(e) {
+        if (e.target.value) {
+            if (!this.hiddenTextValueFlag) {
+                this.hiddenTextValueFlag = true
+                this.textValueEl.className = 'cbd-select-content-textvalue hidden'
+            }
+        } else {
+            this.hiddenTextValueFlag = false
+            this.textValueEl.className = 'cbd-select-content-textvalue'
+
+            console.log(1)
+        }
+    }
+
     // 如果需要绑定事件，需重定义props,如果不需要绑定事件可以直接渲染this.props.children
     renderOpt() {
 
         return (
-            <div className="cbd-select-dropdown">
-                <ul className="cbd-select-dropdown-ul" onClick={this.onSelect.bind(this)} >
+            <div className="cbd-select-dropdown-content" onMouseLeave={this.onMouseLeave.bind(this)} onMouseEnter={this.onMouseEnter.bind(this)}>
+                <ul className="cbd-select-dropdown-ul" onMouseUp={this.onSelect.bind(this)} >
                     {this.props.children}
                 </ul>
             </div>
@@ -90,10 +176,29 @@ class Select extends React.Component {
                 }
                 popup={this.renderOpt.bind(this)}
             >
-                <div style={this.props.style} onClick={this.onClick.bind(this)} onBlur={this.onBlur.bind(this)} className={`cbd-select${this.state.openFlag ? ' open' : ' close'}`} >
-                    <div tabIndex={0}>{this.state.value.value}</div>
-                    <span className="icon" style={{ transform: this.state.openFlag ? 'rotate(-135deg)' : 'rotate(45deg)' }} />
-                </div>
+                {
+                    this.props.showSearch ?
+                        <div style={this.props.style} onMouseLeave={this.onMouseLeaveContent.bind(this)} onMouseEnter={this.onMouseEnterContent.bind(this)} onClick={this.onClick.bind(this)} onBlur={this.onBlur.bind(this)} className={`cbd-select`} >
+                            <div className={`cbd-select-content${this.state.openFlag ? ' activity' : ''}`} tabIndex={0}>
+
+                                <div ref={(el) => { this.textValueEl = el }} className={`cbd-select-content-textvalue`} style={{ opacity: this.state.openFlag ? 0.4 : 1 }} >{this.state.value.value}</div>
+                                {
+                                    this.props.showSearch ? <input ref={(el) => { this.inputEl = el }} className={`${this.state.openFlag ? ' show' : ' hidden'}`} onChange={this.inputOnChange.bind(this)} type="text" /> : ''
+                                }
+                            </div>
+                            <span className="icon" style={{ zIndex: 2, transform: this.state.openFlag ? 'rotate(-135deg)' : 'rotate(45deg)' }} />
+                        </div>
+
+                        :
+                        <div style={this.props.style} onClick={this.onClick.bind(this)} onBlur={this.onBlur.bind(this)} className={`cbd-select`} >
+                            <div className={`cbd-select-content${this.state.openFlag ? ' activity' : ''}`} tabIndex={0}>
+
+                                <div className="cbd-select-content-textvalue">{this.state.value.value}</div>
+                            </div>
+                            <span className="icon" style={{ zIndex: 2, transform: this.state.openFlag ? 'rotate(-135deg)' : 'rotate(45deg)' }} />
+                        </div>
+                }
+
             </Trigger>
         )
     }
@@ -107,7 +212,7 @@ class Option extends React.Component {
     }
     render() {
         return (
-            <li className="cbd-select-dropdown-li" data-key={this.props.value} >{this.props.children}</li>
+            <li className={`cbd-select-dropdown-li${this.props.disabled ? ' disabled' : ''}`} data-key={this.props.value} >{this.props.children}</li>
         )
     }
 }
--- manage/components/webForm/FormComponents.js
+++ manage/components/webForm/FormComponents.js
@@ -33,9 +33,21 @@ class FormComponents extends React.Component {
 
                 <InputSearch style={{ fontSize: '18px', marginTop: '10px' }} onChange={this.textChange.bind(this)} placeholder="请输入姓名" />
                 <InputGroup data={InputGroupData} style={{ fontSize: '16px', marginTop: '10px' }} onChange={this.textChange.bind(this)} placeholder="请输入姓名" />
-                <Select style={{ marginTop: '10px' }} >
-                    <Option value='test' >代理</Option>
+                <Select showSearch style={{ marginTop: '10px' }} >
+                    <Option value='test' >代理1</Option>
                     <Option value='test2' >测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</Option>
+                    <Option value='test' >代理2</Option>
+                    <Option disabled value='test' >代理3</Option>
+                    <Option value='test' >代理4</Option>
+                    <Option value='test' >代理5</Option>
+                    <Option value='test' >代理6</Option>
+                    <Option value='test' >代理7</Option>
+                    <Option value='test' >代理8</Option>
+                    <Option value='test' >代理9</Option>
+                    <Option value='test' >代理0</Option>
+                    <Option value='test' >代理1</Option>
+                    <Option value='test' >代理2</Option>
+                    <Option value='test' >代理3</Option>
                 </Select>
                 <TableReport data={TableReportData} columns={TableReportColumn} style={{ marginTop: '10px' }} />
             </div>
